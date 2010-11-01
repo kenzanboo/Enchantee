3785a9c9a06608764ae8086bad7bf3dee36b9c32
Serban Giuroiu <giuroiu@berkeley.edu>
2010-10-31 22:26:16 -0700

    Add a placeholder controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   
   map.resource :user_session
-  #map.login "login", :controller => "user_sessions", :action => "new"
-  #map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -41,7 +41,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "placeholder", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
