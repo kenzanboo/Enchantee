d84be2f9fa668ee67bc75dae4e60faf66fb2db29
Serban Giuroiu <giuroiu@berkeley.edu>
2010-11-21 22:43:47 -0800

    Remove the index action from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,6 @@ class UsersController < ApplicationController
       format.xml  { render :xml => @users }
     end
   end
-  
-  # TODO: PENDING REMOVAL
-  # GET /users
-  # GET /users.xml
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
-  end
 
   # GET /users/1
   # GET /users/1.xml
