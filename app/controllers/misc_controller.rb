b8cf24eed0b671efd08b38939048a97124db9573
Serban Giuroiu <giuroiu@berkeley.edu>
2010-11-21 20:31:41 -0800

    Add the login form to the welcome page

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,4 +1,6 @@
 class MiscController < ApplicationController
   def welcome
+    # TODO: This isn't very DRY, but it's necessary to make the login form partial work
+    @user_session = UserSession.new
   end
 end
