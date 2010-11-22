d864d5d0ea19f03f509054fef129a155ee2d5ec5
Serban Giuroiu <giuroiu@berkeley.edu>
2010-11-22 00:20:17 -0800

    Remove the cleartext password field from the database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101122064138) do
+ActiveRecord::Schema.define(:version => 20101122080929) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(:version => 20101122064138) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "password"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "photo"
