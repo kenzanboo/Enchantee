77e2f2d2a972863873e02b43c4ac502c4a66d3df
Ryan <ry4n1m3@gmail.com>
2010-12-12 23:24:23 -0800

    fixed adding yourself as a friend and style change on user_short

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  def validate
+  	errors.add_to_base("You cannot save your own profile.") unless user_id != friend_id
+  end
 end
